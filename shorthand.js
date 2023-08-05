e5c5bacc5bb4bb996ce9d2df0938605f33440139
Bluerana377 <hasansohel492@gmail.com>
2023-08-05 18:44:42 +0400

    added the shortand.js file

---
+++ shorthand.js
@@ -0,0 +1,45 @@
+// variable  declarations
+var tomatoPrice = 25;
+// variable value update
+tomatoPrice = 30;
+
+// add something to the previous value
+var price1 = 35;
+var price2 = 10;
+
+// set a new value  
+price1 = 35;
+
+// add some value to the existing value 
+price1 = price1 + 10;
+// console.log(price1)
+
+// addition to the some variable shorthand 
+// += adding some value to the existing value
+ price1 += 10;
+//  console.log(price1)
+
+ price2 = price2 - 10;
+
+ // shorthand
+ // -= means deducting some value to the current value of the variable 
+ price2 -= 2;
+
+ // *= 
+price1 *= 5;
+// console.log(price1)
+
+// /=
+price1 /= 5;
+// console.log(price1)
+
+// ++ means adding 1 to the existing value
+var age = 16;
+age = age + 1;
+age++;
+console.log(age);
+
+var love = 100;
+love = love -1;
+// -- means you are deducting to the existing value
+love--;
