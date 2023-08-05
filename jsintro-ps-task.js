ac81c768af8ed9297c6ea045db2cd4a74083f352
Bluerana377 <hasansohel492@gmail.com>
2023-08-05 22:14:45 +0400

    complete the module 1

---
+++ jsintro-ps-task.js
@@ -0,0 +1,22 @@
+// practice problem - 1
+
+// var momGiveTotal = 2000;
+// var appleCost = 1200;
+// var orangeCost = 300;
+// var totalCost = appleCost + orangeCost;
+// var momGiveTotalDeduct = momGiveTotal - totalCost;
+// console.log('total apple cost' + ' ' + appleCost + ' ' + 'total Orange cost'+ orangeCost + ' ' + 'grandTotal'+ ' ' + totalCost + ' ' + 'in harrys hand total' + ' ' + momGiveTotalDeduct);
+
+// practice problem - 2
+
+
+// practice problem - 3
+// var firstLine = "I am going to be";
+// var secondLine = "an awesome web developer";
+// console.log(firstLine+ ' ' + secondLine);
+
+// practice problem - 4
+var num1 = 17;
+var num2 = 5;
+var reminder = num1 % num2;
+console.log(reminder);
---
+++ remainder.js
@@ -0,0 +1,8 @@
+var mangos = 19;
+var person = 7;
+var reminder = mangos % person;
+
+// / this means vagfol
+// % this means vagsesh
+
+console.log(reminder);
