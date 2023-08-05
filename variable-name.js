ca69f3ef5b8461b719a6d36d47691400364ec9fa
Bluerana377 <hasansohel492@gmail.com>
2023-08-05 11:20:51 +0400

    added more js file

---
+++ variable-name.js
@@ -0,0 +1,32 @@
+// right Variable 
+var price = 48;
+
+// 1. variable name can not be keywords
+// Example
+var false = 78;
+var return = true; // Wrong way to declare a variable with keywords
+
+// 2. can not use more then one words to declare a variable name
+var my home address = "london";
+
+// 3. Variable name can not have quotation 
+var name = "tom hank";
+
+// 4. variable name  can not start with number can not ends a number
+var 99club = 8838;
+var club88 = 3773;
+
+// 5. How to use long names 
+// can not use dash 
+var user-name = "raj";
+
+var user_name = "raj";
+
+var user_home_address = "andor killa bandor bo";
+var userhomeaddress = "andor killa bandor bo"; // snake case
+var userHomeAddress = "andor killa bandor bo"; // camel case: we will use this one
+var user_home_address = "andor killa bandor bo"; // pascal case
+
+// variable name is case sensitive 
+var person = 25;
+var Person = 376;
