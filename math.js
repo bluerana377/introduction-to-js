a401acf4d6bbbffea0291f66e3c78a48eab19dd4
Bluerana377 <hasansohel492@gmail.com>
2023-08-05 10:59:52 +0400

    added some js files

---
+++ math.js
@@ -0,0 +1,17 @@
+/* 
+1, Number ==> 55, 577, -32
+2, Integer, odd, even: 2,3,
+3, float: decimal: 2.5, 13.5 74.6
+4, Operation: addition, substraction, multiplication, division,       remainder.
+5, percent
+6, average
+7, comparison: >,<, ==,!=,  =>, <=
+8, oikik: 2 kilo apple and 5 kilo mango 
+9, 45, 25, 57 , 83, 9393: min, max, avg, sum
+10, series:
+:: Extra::
+ 1. Prime
+ 2. permutation
+ :::: ALwyas:::: 
+ proujone ami shikhe nibo 
+*/
---
+++ variable.js
@@ -0,0 +1,18 @@
+// very + able = variable
+
+// numeric
+var price = 12;
+var age = 21;
+var temperature = 30;
+var wight = 61;
+var total = 10000;
+// string 
+var gain = "books";
+var person = "sundor uddin";
+var location = "andor killa bandorbon";
+var special = 'allia bhat';
+
+// Boolean 
+var serious = true;
+var isFullMark = true;
+var areYouSingle = false;
