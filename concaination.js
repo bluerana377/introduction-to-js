e58a42e5b093fdb04d809d9108766a49a13307d6
Bluerana377 <hasansohel492@gmail.com>
2023-08-05 21:00:45 +0400

    added some js file

---
+++ concaination.js
@@ -0,0 +1,9 @@
+var price1 = 42;
+var price2 = 8;
+var totalPrice = price1 + price2;
+// console.log(totalPrice)
+
+var firstName = 'jannie';
+var lastName = 'kim';
+var fullName = firstName + ' ' + lastName;
+console.log(fullName);
---
+++ types.js
@@ -0,0 +1,4 @@
+var price = 100;
+// console.log(typeof price)
+var price = '100';
+console.log(typeof price)
