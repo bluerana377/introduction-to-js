3cddc10a97bd1bd1f963488fd74719c6f8235ed4
Bluerana377 <hasansohel492@gmail.com>
2023-08-05 18:19:05 +0400

    added the operations operand in js

---
+++ operation.js
@@ -0,0 +1,25 @@
+var onionPrice = 40;
+var eggPrice = 12;
+
+// // addition, And Substraction 
+
+var totalPrice = onionPrice + eggPrice;
+var priceDeferent = onionPrice - eggPrice;
+
+// console.log(priceDeferent)
+
+// console.log(onionPrice)
+// console.log(eggPrice)
+// console.log(onionPrice + eggPrice)
+
+// Multiplication 
+var orangePrice = 30;
+var quantity = 10;
+var totalCost = orangePrice * quantity;
+// console.log(totalCost) 
+
+// Division 
+var money = 1000;
+var player = 5;
+var eachPlayer = money / player;
+console.log(eachPlayer)
